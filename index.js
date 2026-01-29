9023390808126870e51d8ac5859db31022907b6d
leoliuxin <leoliuxinxin@gmail.com>
2026-01-29 17:59:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活
 const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
 const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '19de68ba-90e8-493a-a889-d6b35055f449'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const UUID = process.env.UUID || 'b529ae01-7504-439f-9706-cbe94c329bf3'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'galaxy2.uncleleo.ccwu.cc';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZjE1YTc0NjZlOGY1YTk0ZWNjZTRlMmM0YmE3NmM2ZWQiLCJ0IjoiOGI3YWJjOTYtNGUzNS00YTBkLTgzZDEtYjRkYzhmNzhlYTFmIiwicyI6IllqazVaalJoWm1RdFpqUmpPQzAwTURZeExXRmpOR1F0WVdRM09UTTVOREF6Tm1VNSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'wispbyte.leouncle.cc.cd';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZjE1YTc0NjZlOGY1YTk0ZWNjZTRlMmM0YmE3NmM2ZWQiLCJ0IjoiYTU3YzViZGUtYWRmYy00OTc2LWIzMGItOGZhZTA4MTc4ZjA4IiwicyI6IlpXWTBZVFprTkRBdE1UY3paUzAwTUdObUxUZ3pZVGt0WlRFNU9EWXhOVGMwT1RjMyJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'Galaxy';                        // 节点名称
+const NAME = process.env.NAME || 'Wispbyte';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
